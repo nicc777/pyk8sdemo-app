7a7159657ba62ee290324c9ec68bae071fa3fd0f
Nico Coetzee <nicc777@gmail.com>
2022-04-09 12:32:08 +0200

    test: Added a locust test file for simulating testing the app running on three nodes.

---
+++ test/locust/basic_hunger_test.py
@@ -0,0 +1,51 @@
+from locust import HttpUser, task, between
+import os
+
+HOST1 = os.getenv('HOST1', '127.0.0.1')
+HOST2 = os.getenv('HOST2', '127.0.0.1')
+HOST3 = os.getenv('HOST3', '127.0.0.1')
+
+
+class node1(HttpUser):
+
+    host = 'http://{}:8880'.format(HOST1)
+    wait_time = between(2, 4)
+
+    @task()
+    def node_t1(self):
+        self.client.get('/')
+
+    @task()
+    def node_t2(self):
+        self.client.get('/hungry')
+
+
+class node2(HttpUser):
+
+    host = 'http://{}:8880'.format(HOST2)
+    wait_time = between(2, 4)
+
+    @task()
+    def node_t1(self):
+        self.client.get('/')
+
+    @task()
+    def node_t2(self):
+        self.client.get('/hungry')
+
+
+class node3(HttpUser):
+
+    host = 'http://{}:8880'.format(HOST3)
+    wait_time = between(2, 4)
+
+    @task()
+    def node_t1(self):
+        self.client.get('/')
+
+    @task()
+    def node_t2(self):
+        self.client.get('/hungry')
+
+        
+        
